28e8f9a81ef540fbde1c959a518cae184136036b
Pavel Uvarov <pavel_uvarov@epam.com>
2020-05-04 11:10:41 +0300

    add fauna db

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import Axios from 'axios'
 import {useState } from 'react';
 import './App.css';
+import { useEffect } from 'react/cjs/react.production.min';
 
 function App() {
   const [level, setLevel] = useState(null); 
+
+  useEffect(() => {
+    Axios.get('/api/db').then(res => console.log(res))
+  });
   return (
     <main>
       <input value={level} onChange={e => setLevel(e.target.value)} />
