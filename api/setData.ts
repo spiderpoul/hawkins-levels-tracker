d6271c4118dcbe4ac72c5e3decf38e5d0e174bc8
Pavel Uvarov <pavel_uvarov@epam.com>
2020-05-07 03:50:55 +0300

    remove unused endpoint

--- api/setData.ts
+++
@@ -1,9 +0,0 @@
-import { NowRequest, NowResponse } from '@now/node';
-
-const data:any = [];
-
-export default (_req: NowRequest, res: NowResponse) => {
-  const {body} = _req;
-  data.push(body);
-  res.status(200).send(data);
-};
